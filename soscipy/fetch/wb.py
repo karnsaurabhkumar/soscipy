e020564c4d8b3b57b81e7f23dc181460aa001e7c
karnsaurabhkumar <karnsaurabhkumar@gmail.com>
2021-04-15 10:03:27 +0530

    Modified the World Bank data fetcher

--- soscipy/fetch/wb.py
+++ soscipy/fetch/wb.py
@@ -2,15 +2,15 @@ import world_bank_data as wb
 from soscipy.process import dfops
 
 
-def world_bank_data(url):
+def world_bank_data(url, date):
     """
     Takes a URL for input and extracts the indicator string. This is then used to extract data from world bank data
     :param url: URL of the data page
     :return: Dataframew with indicator as the last column
     """
     indicator = url.split('?')[0].split('/')[-1]
-    data = wb.get_series(indicator,mrv=1).to_frame().reset_index()
+    data = wb.get_series(indicator, date=date, mrv=1).to_frame().reset_index()
     series = data['Series'].unique()[0]
-    data = data.drop(['Series'],axis=1)
-    data = dfops.rename_pd(data,[data.columns[-1]],[series])
+    data = data.drop(['Series'], axis=1)
+    data = dfops.rename_pd(data, [data.columns[-1]], [series])
     return data
